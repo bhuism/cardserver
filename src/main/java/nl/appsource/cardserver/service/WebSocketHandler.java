4aabd9d46d240379ee83e415a8e3c0bdba62d647
Bas Huisman <bhuism@odee.net>
2025-04-17 14:08:56 +0200

    move to JSON

--- src/main/java/nl/appsource/cardserver/service/WebSocketHandler.java
+++ src/main/java/nl/appsource/cardserver/service/WebSocketHandler.java
@@ -1,14 +1,15 @@
 package nl.appsource.cardserver.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import org.springframework.web.util.HtmlUtils;
 
 import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 @Component
@@ -18,12 +19,12 @@ public class WebSocketHandler extends TextWebSocketHandler {
     private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
 
     @Override
-    public void handleTransportError(final WebSocketSession session, final Throwable throwable) throws Exception {
+    public void handleTransportError(final WebSocketSession session, final Throwable throwable) {
         log.error("error occured at sender " + session, throwable);
     }
 
     @Override
-    public void afterConnectionClosed(final WebSocketSession session, final CloseStatus status) throws Exception {
+    public void afterConnectionClosed(final WebSocketSession session, final CloseStatus status) {
         log.info(String.format("Session %s closed because of %s", session.getId(), status.getReason()));
         sessions.remove(session);
     }
@@ -39,12 +40,21 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(final WebSocketSession session, final TextMessage message) throws Exception {
-        String request = message.getPayload();
-        log.info("Server received: {}", request);
+        final String request = message.getPayload();
 
-        String response = String.format("response from server to '%s'", HtmlUtils.htmlUnescape(request));
-        log.info("Server sends: {}", response);
-        session.sendMessage(new TextMessage(response));
+        final JSONObject requestJSON = new JSONObject(request);
+
+        log.info("Server #session: {}, received: {}", sessions.size(), requestJSON);
+
+//        final String response = String.format("response from server to '%s'", HtmlUtils.htmlUnescape(request));
+
+//        log.info("Server sends: {}", response);
+
+        final JSONObject payload = new JSONObject();
+
+        payload.put("response", UUID.randomUUID().toString());
+
+        session.sendMessage(new TextMessage(payload.toString()));
     }
 
 }
