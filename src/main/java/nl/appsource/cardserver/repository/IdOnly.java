eb944261e39f86b5141c7cffed5f5f2be0134886
Bas Huisman <bhuism@odee.net>
2025-05-18 22:36:19 +0200

    cleanups

--- src/main/java/nl/appsource/cardserver/repository/IdOnly.java
+++
@@ -1,9 +0,0 @@
-package nl.appsource.cardserver.repository;
-
-import org.springframework.beans.factory.annotation.Value;
-
-public interface IdOnly {
-
-    @Value("#{target.id}")
-    String getId();
-}
