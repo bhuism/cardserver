f42e4d6f157475b9a0471b66e54ccd2682f3a3c2
Bas Huisman <bhuism@odee.net>
2025-06-10 14:07:09 +0200

    checkstyle

--- src/main/java/nl/appsource/cardserver/model/Game.java
+++ src/main/java/nl/appsource/cardserver/model/Game.java
@@ -13,7 +13,6 @@ import org.springframework.data.couchbase.core.mapping.Document;
 import org.springframework.data.couchbase.core.mapping.Field;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
