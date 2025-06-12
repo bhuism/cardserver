5b0cc5fa7c456386805b26a46ffbc02c09fba6d2
Bas Huisman <bhuism@odee.net>
2025-06-12 16:45:27 +0200

    better emitter

--- src/main/java/nl/appsource/cardserver/controller/SSEController.java
+++ src/main/java/nl/appsource/cardserver/controller/SSEController.java
@@ -6,28 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 @Slf4j
 @Controller
 public class SSEController {
 
-    private final ExecutorService executor = Executors.newCachedThreadPool();
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
     @GetMapping("/websocket")
-    public SseEmitter streamSseEvents() {
-        final SseEmitter emitter = new SseEmitter(Long.MAX_VALUE); // Long-lived connection
-        executor.execute(() -> {
-            try {
-                Thread.sleep(1000); // simulate delay
-                log.info("Sending SSE message ");
-                emitter.send("SSE MVC - " + System.currentTimeMillis());
-                emitter.complete();
-            } catch (IOException | InterruptedException e) {
-                emitter.completeWithError(e);
-            }
+    public SseEmitter streamSseEvents() throws IOException {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        emitters.add(emitter);
+        emitter.send("SSE MVC - " + System.currentTimeMillis());
+        emitter.onCompletion(() -> {
+            log.info("onCompletion() Removing an emitier");
+            emitters.remove(emitter);
+        });
+        emitter.onTimeout(() -> {
+            log.info("onTimeout() Removing an emitier");
+            emitters.remove(emitter);
         });
         return emitter;
     }
