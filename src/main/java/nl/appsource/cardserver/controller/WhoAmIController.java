8f514fbd12e8ac190e7555bf1efcdf917c402ec9
Bas Huisman <bhuism@odee.net>
2025-06-12 15:20:53 +0200

    make whoami a post to /whoami

--- src/main/java/nl/appsource/cardserver/controller/WhoAmIController.java
+++ src/main/java/nl/appsource/cardserver/controller/WhoAmIController.java
@@ -2,6 +2,7 @@ package nl.appsource.cardserver.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import nl.appsource.cardserver.converter.UserToOpenApiConverter;
 import nl.appsource.cardserver.filter.LoggingFilter;
 import nl.appsource.cardserver.service.UserService;
 import org.openapitools.api.WhoamiApi;
@@ -11,16 +12,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 public class WhoAmIController implements WhoamiApi {
 
     private final UserService userService;
+
+    private final UserToOpenApiConverter userToOpenApiConverter;
 
     @Override
     public ResponseEntity<User> whoami() {
@@ -33,6 +34,7 @@ public class WhoAmIController implements WhoamiApi {
         LoggingFilter.requestLogMessage(("whoampi(), email=" + email));
 
         return userService.findByEmail(email)
+            .map(userToOpenApiConverter::convert)
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 
