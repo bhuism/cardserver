9b67bfe87e642ae5fd45ad13378ed69b844f4021
Bas Huisman <bhuism@odee.net>
2025-06-12 09:26:25 +0200

    sse update

--- src/main/java/nl/appsource/cardserver/controller/SSEController.java
+++ src/main/java/nl/appsource/cardserver/controller/SSEController.java
@@ -1,5 +1,6 @@
 package nl.appsource.cardserver.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -9,6 +10,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
+@Slf4j
 @Controller
 public class SSEController {
 
@@ -19,10 +21,9 @@ public class SSEController {
         final SseEmitter emitter = new SseEmitter(Long.MAX_VALUE); // Long-lived connection
         executor.execute(() -> {
             try {
-                for (int i = 0; i < 10; i++) {
-                    Thread.sleep(1000); // simulate delay
-                    emitter.send("SSE MVC - " + System.currentTimeMillis());
-                }
+                Thread.sleep(1000); // simulate delay
+                log.info("Sending SSE message ");
+                emitter.send("SSE MVC - " + System.currentTimeMillis());
                 emitter.complete();
             } catch (IOException | InterruptedException e) {
                 emitter.completeWithError(e);
