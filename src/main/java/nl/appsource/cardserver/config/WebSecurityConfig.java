198e70028b16bd5e69e06237f2da50eac814c3b9
Bas Huisman <bhuism@odee.net>
2025-06-13 21:41:56 +0200

    checkstyle

--- src/main/java/nl/appsource/cardserver/config/WebSecurityConfig.java
+++ src/main/java/nl/appsource/cardserver/config/WebSecurityConfig.java
@@ -1,13 +1,9 @@
 package nl.appsource.cardserver.config;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
