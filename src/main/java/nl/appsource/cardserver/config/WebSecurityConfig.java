db73ef97fd087bab0f711af0ef3ccf82ad5e0638
Bas Huisman <bhuism@odee.net>
2025-06-13 21:41:24 +0200

    cleanups

--- src/main/java/nl/appsource/cardserver/config/WebSecurityConfig.java
+++ src/main/java/nl/appsource/cardserver/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package nl.appsource.cardserver.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import jakarta.servlet.DispatcherType;
@@ -42,13 +41,12 @@ public class WebSecurityConfig {
 
     private final CardSeverAuthFilter cardSeverAuthFilter;
 
-    @Autowired
-    void configureObjectMapper(final ObjectMapper mapper) {
-        log.info("Registering JSR310 for jackson serialisation");
-        mapper.registerModule(new ParameterNamesModule())
-            .registerModule(new Jdk8Module())
-            .registerModule(new JavaTimeModule());
-    }
+//    @Autowired
+//    void configureObjectMapper(final ObjectMapper mapper) {
+//        log.info("Registering JSR310 for jackson serialisation");
+//        mapper.registerModule(new ParameterNamesModule())
+//            .registerModule(new JavaTimeModule());
+//    }
 
 
 //    @Bean
