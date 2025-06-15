7bd4f75f8a4fea841dcc9a66caad0770f1c59e9e
Bas Huisman <bhuism@odee.net>
2025-06-15 21:59:39 +0200

    fix whoamiapi

--- src/main/java/nl/appsource/cardserver/controller/WhoAmIController.java
+++ src/main/java/nl/appsource/cardserver/controller/WhoAmIController.java
@@ -1,13 +1,13 @@
 package nl.appsource.cardserver.controller;
 
-import com.nimbusds.jwt.SignedJWT;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import nl.appsource.cardserver.converter.UserToOpenApiConverter;
 import nl.appsource.cardserver.filter.LoggingFilter;
-import nl.appsource.cardserver.model.User;
 import nl.appsource.cardserver.service.CardServerJwtModem;
 import nl.appsource.cardserver.service.UserService;
 import org.openapitools.api.WhoamiApi;
+import org.openapitools.model.Whoami200Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -29,9 +29,12 @@ public class WhoAmIController implements WhoamiApi {
     private final UserService userService;
 
     private final CardServerJwtModem cardServerJwtModem;
+    private final UserToOpenApiConverter userToOpenApiConverter;
+
 
     @Override
-    public ResponseEntity<String> whoami() {
+    public ResponseEntity<Whoami200Response> whoami() {
+
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         final Jwt principal = (Jwt) authentication.getPrincipal();
@@ -62,10 +65,8 @@ public class WhoAmIController implements WhoamiApi {
                     return userService.save(user);
 
                 }))
-            .map(User::getId)
-//            .map(userToOpenApiConverter::convert)
-            .map(cardServerJwtModem::encode)
-            .map(SignedJWT::serialize)
+            .map(userToOpenApiConverter::convert)
+            .map((user) -> new Whoami200Response().user(user).jwt(cardServerJwtModem.encode(user.getId()).serialize()))
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 
