b56ce7d94bed7f49f937603d976185f7039924f9
bhuism <bhuism@odee.net>
2025-06-19 21:20:41 +0200

    find all id

--- src/main/java/nl/appsource/cardserver/service/UserServiceImpl.java
+++ src/main/java/nl/appsource/cardserver/service/UserServiceImpl.java
@@ -3,14 +3,12 @@ package nl.appsource.cardserver.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import nl.appsource.cardserver.converter.UserToOpenApiConverter;
 import nl.appsource.cardserver.model.User;
 import nl.appsource.cardserver.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +17,6 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    private final UserToOpenApiConverter userToOpenApiConverter;
-
     @Override
     public Optional<User> findById(final String userId) {
         return userRepository.findById(userId);
@@ -28,7 +24,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getUsers(final List<String> userIds) {
-        return user
+        return userRepository.findAllById(userIds);
     }
 
     @Override
@@ -39,7 +35,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findAllIncomingInvites(final String userId) {
-        return userRepository.findAllIncomingInvites(userId).stream().collect(Collectors.toList());
+        return userRepository.findAllIncomingInvites(userId);
     }
 
     @Override
