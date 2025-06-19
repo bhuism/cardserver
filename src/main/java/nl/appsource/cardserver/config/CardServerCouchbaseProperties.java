df8c9df2f1454c01609d19981da7a7352154ecce
bhuism <bhuism@odee.net>
2025-06-19 21:29:40 +0200

    add role infrastructure

--- src/main/java/nl/appsource/cardserver/config/CardServerCouchbaseProperties.java
+++ src/main/java/nl/appsource/cardserver/config/CardServerCouchbaseProperties.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Role;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.beans.factory.config.BeanDefinition.ROLE_INFRASTRUCTURE;
 
 @Component
 @ConfigurationProperties(prefix = "cardserver.couchbase")
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Component;
 @Setter
 @RequiredArgsConstructor
 @Accessors(chain = true)
+@Role(ROLE_INFRASTRUCTURE)
 public final class CardServerCouchbaseProperties {
 
     private String connectionString;
