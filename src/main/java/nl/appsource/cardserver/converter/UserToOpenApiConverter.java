e6ca3ffbc4919206d6250149b03093ccf97b89a3
bhuism <bhuism@odee.net>
2025-06-23 22:54:53 +0200

    less User fields

--- src/main/java/nl/appsource/cardserver/converter/UserToOpenApiConverter.java
+++ src/main/java/nl/appsource/cardserver/converter/UserToOpenApiConverter.java
@@ -5,8 +5,6 @@ import nl.appsource.cardserver.model.User;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @RequiredArgsConstructor
 @Component
 public class UserToOpenApiConverter implements Converter<User, org.openapitools.model.User> {
@@ -17,14 +15,14 @@ public class UserToOpenApiConverter implements Converter<User, org.openapitools.
         final org.openapitools.model.User target = new org.openapitools.model.User();
 
         target.setId(source.getId());
-        target.setCreated(source.getCreated());
-        target.setEmail(source.getEmail());
+//        target.setCreated(source.getCreated());
+//        target.setEmail(source.getEmail());
         target.setUpdated(source.getUpdated());
         target.setDisplayName(source.getDisplayName());
-        target.setLastLogin(Optional.ofNullable(source.getLastLogin()));
-        target.setName(source.getName());
+//        target.setLastLogin(Optional.ofNullable(source.getLastLogin()));
+//        target.setName(source.getName());
         target.setPhotoURL(source.getPhotoURL());
-        target.setProviderId(source.getProviderId());
+//        target.setProviderId(source.getProviderId());
 
         return target;
     }
