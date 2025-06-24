e6c5b8df58647c3fa268baa9833d42a85c4619ae
bhuism <bhuism@odee.net>
2025-06-24 23:00:16 +0200

    checkstyle

--- src/main/java/nl/appsource/cardserver/service/GameServiceImpl.java
+++ src/main/java/nl/appsource/cardserver/service/GameServiceImpl.java
@@ -19,7 +19,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static java.lang.Math.abs;
