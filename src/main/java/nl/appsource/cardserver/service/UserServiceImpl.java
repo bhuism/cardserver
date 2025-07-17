44666d03a03add6e0ff95754fd9d26338df38187
bhuism <bhuism@odee.net>
2025-07-17 18:34:31 +0200

    better accept friends online

--- src/main/java/nl/appsource/cardserver/service/UserServiceImpl.java
+++ src/main/java/nl/appsource/cardserver/service/UserServiceImpl.java
@@ -71,6 +71,7 @@ public class UserServiceImpl implements UserService {
 
         }).flatMap(userRepository::save).flatMap((user) -> {
             sseEmitterRepository.friendsChanged(Set.of(friendId, user.getId()));
+            sseEmitterRepository.sendOnlineListToFriendsOf(friendId);
             return Mono.empty();
         });
     }
