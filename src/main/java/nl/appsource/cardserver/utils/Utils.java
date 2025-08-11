ad3e696d5bf681f8696463a9192b805ea4316057
bhuism <bhuism@odee.net>
2025-08-11 10:18:48 +0200

    less admins

--- src/main/java/nl/appsource/cardserver/utils/Utils.java
+++ src/main/java/nl/appsource/cardserver/utils/Utils.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public final class Utils {
 
-    private static final Set<String> ADMINS = Set.of("BbdQWhNJjyfawQhWwWDv1aXncQu2", "J1Z2dqmV5Q68ikGLOvk1t8yIVoWX", "GwsuFaXvG4dEvVIUhbpwLuQ4DYu1");
+    private static final Set<String> ADMINS = Set.of("YqI3QttWmwRKgyTIX9Idok1admqp");
 
     public static boolean isAdmin(final String userId) {
         return ADMINS.contains(userId);
