b4d70bf3a959e700bb48118bf4d952d9cc4137d1
Bas <bhuism@users.noreply.github.com>
2025-08-27 23:30:50 +0200

    Update GameServiceImpl.java

--- src/main/java/nl/appsource/cardserver/service/GameServiceImpl.java
+++ src/main/java/nl/appsource/cardserver/service/GameServiceImpl.java
@@ -279,7 +279,7 @@ public class GameServiceImpl implements GameService {
                                 default -> true;
                             };
                         }));
-        }).share();
+        });
     }
 
 
