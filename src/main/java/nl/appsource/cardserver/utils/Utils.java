aa594be71b2fb66efc74e594e8a3dfee3d447153
Bas Huisman <bhuism@odee.net>
2025-09-04 12:33:11 +0200

    less admin
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- src/main/java/nl/appsource/cardserver/utils/Utils.java
+++ src/main/java/nl/appsource/cardserver/utils/Utils.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public final class Utils {
 
-    private static final Set<String> ADMINS = Set.of("YqI3QttWmwRKgyTIX9Idok1admqp", "BbdQWhNJjyfawQhWwWDv1aXncQu2");
+    private static final Set<String> ADMINS = Set.of("YqI3QttWmwRKgyTIX9Idok1admqp");
 
     public static boolean isAdmin(final String userId) {
         return ADMINS.contains(userId);
