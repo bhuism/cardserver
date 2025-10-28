0d7fc9c1c2519806bf6fa4affef4de39f4671b9a
bhuism <bhuism@odee.net>
2025-10-28 21:53:53 +0100

    add boom

---
+++ src/main/java/nl/appsource/cardserver/model/Boom.java
@@ -0,0 +1,49 @@
+package nl.appsource.cardserver.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.openapitools.model.GameVariant;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.couchbase.core.mapping.Document;
+import org.springframework.data.couchbase.core.mapping.Field;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Document
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Boom {
+
+    @Id
+    private String id;
+
+    @Field
+    @NotNull
+    private Instant created;
+
+    @Field
+    @NotNull
+    private Instant updated;
+
+    @NotNull
+    @Field
+    private GameVariant gameVariant = GameVariant.ROTTERDAMS;
+
+    @NotNull
+    @Field
+    private Integer dealer;
+
+    @Field
+    @NotNull
+    private List<String> players = new ArrayList<>();
+
+}
