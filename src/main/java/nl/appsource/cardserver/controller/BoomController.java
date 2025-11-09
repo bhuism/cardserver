e0cb9df85f047e5212140569bbed919a8bfc6e4a
bhuism <bhuism@odee.net>
2025-11-09 11:26:17 +0100

    return game in playboom

--- src/main/java/nl/appsource/cardserver/controller/BoomController.java
+++ src/main/java/nl/appsource/cardserver/controller/BoomController.java
@@ -2,6 +2,7 @@ package nl.appsource.cardserver.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import nl.appsource.cardserver.converter.BoomToOpenApiConverter;
+import nl.appsource.cardserver.converter.GameToOpenApiConverter;
 import nl.appsource.cardserver.repository.BoomRepository;
 import nl.appsource.cardserver.repository.GameRepository;
 import nl.appsource.cardserver.service.BoomService;
@@ -11,16 +12,13 @@ import nl.appsource.cardserver.service.SseEmitterRepository;
 import org.openapitools.api.BoomApi;
 import org.openapitools.model.Boom;
 import org.openapitools.model.CreateBoom;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.openapitools.model.Game;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -37,13 +35,16 @@ public class BoomController extends GenericController implements BoomApi {
 
     private final GameService gameService;
 
-    public BoomController(final SseEmitterRepository sseEmitterRepository, final BoomService boolServiceArg, final BoomToOpenApiConverter boomToOpenApiConverterArg, final GameRepository gameRepositoryArg, final BoomRepository boomRepositoryArg, final GameService gameServiceArg) {
+    private final GameToOpenApiConverter gameToOpenApiConverter;
+
+    public BoomController(final SseEmitterRepository sseEmitterRepository, final BoomService boolServiceArg, final BoomToOpenApiConverter boomToOpenApiConverterArg, final GameRepository gameRepositoryArg, final BoomRepository boomRepositoryArg, final GameService gameServiceArg, final GameToOpenApiConverter gameToOpenApiConverterArg) {
         super(sseEmitterRepository);
         this.boomService = boolServiceArg;
         this.boomToOpenApiConverter = boomToOpenApiConverterArg;
         this.gameRepository = gameRepositoryArg;
         this.boomRepository = boomRepositoryArg;
         this.gameService = gameServiceArg;
+        this.gameToOpenApiConverter = gameToOpenApiConverterArg;
     }
 
     @Override
@@ -87,8 +88,9 @@ public class BoomController extends GenericController implements BoomApi {
                 .build());
     }
 
+
     @Override
-    public Mono<ResponseEntity<Void>> playBoom(final UUID appIdentifier, final String boomId, final ServerWebExchange exchange) {
+    public Mono<ResponseEntity<Game>> playBoom(final UUID appIdentifier, final String boomId, final ServerWebExchange exchange) {
         return authorize(appIdentifier, exchange)
             .doOnNext((userId) -> log.info("{} getBoom() userId={} boomId={}", exchange.getRequest()
                 .getRemoteAddress(), userId, boomId))
@@ -105,14 +107,16 @@ public class BoomController extends GenericController implements BoomApi {
                                     return gameService.createGame(userId, boom.getPlayers(), boom.getId())
                                         .doOnNext(game -> boom.getGames()
                                             .add(game.getId()))
-                                        .flatMap(game -> boomRepository.save(boom).thenReturn(game));
-                                        //.zipWith(boomRepository.save(boom))
-                                        //.map(Tuple2::getT1)
+                                        .flatMap(game -> boomRepository.save(boom)
+                                            .thenReturn(game));
+                                    //.zipWith(boomRepository.save(boom))
+                                    //.map(Tuple2::getT1)
                                 } else {
                                     return Mono.empty();
                                 }
                             }))
-                            .map(game -> new ResponseEntity<Void>(new HttpHeaders(MultiValueMap.fromSingleValue(Map.of("Location", "https://klaversjassen.nl/gamePlay/" + game.getId()))), HttpStatus.FOUND))
+                            .mapNotNull(gameToOpenApiConverter::convert)
+                            .map(ResponseEntity::ok)
                             .defaultIfEmpty(ResponseEntity.notFound()
                                 .build());
                     });
