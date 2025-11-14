a055ae61fd813a5c4a4b231ec630739e2cef10d2
bhuism <bhuism@odee.net>
2025-11-14 11:15:03 +0100

    test ReactiveBeforeConvertCallback

---
+++ src/main/java/nl/appsource/cardserver/repository/BeforeSaveEventGame.java
@@ -0,0 +1,23 @@
+package nl.appsource.cardserver.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import nl.appsource.cardserver.model.Game;
+import org.reactivestreams.Publisher;
+import org.springframework.data.couchbase.core.mapping.event.ReactiveBeforeConvertCallback;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+
+@Slf4j
+@Component
+public class BeforeSaveEventGame implements ReactiveBeforeConvertCallback<Game> {
+    @Override
+    public Publisher<Game> onBeforeConvert(Game game, String collection) {
+        log.info("Before save: " + game.getId());
+        game.setUpdated(Instant.now());
+
+        return Mono.just(game);
+    }
+
+}
