1e14e70be8fa464d4f7f4248c99aa7ff032ecdc8
Bas Huisman <bhuism@odee.net>
2025-11-26 10:19:19 +0100

    remove thread sleep on subscribe
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- src/main/java/nl/appsource/cardserver/service/SseEmitterRepositoryImpl.java
+++ src/main/java/nl/appsource/cardserver/service/SseEmitterRepositoryImpl.java
@@ -19,7 +19,6 @@ import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
@@ -246,11 +245,11 @@ public class SseEmitterRepositoryImpl implements SseEmitterRepository {
     @Override
     public Flux<ServerSentEvent<?>> subscribe(final UUID appIdentifier, final String userId, final String remoteAddress) {
 
-        try {
-            Thread.sleep(Duration.ofSeconds(2));
-        } catch (InterruptedException e) {
-            log.error("SLeep interrupted", e);
-        }
+//        try {
+//            Thread.sleep(Duration.ofSeconds(2));
+//        } catch (InterruptedException e) {
+//            log.error("SLeep interrupted", e);
+//        }
 
         final MySseEmitter mySseEmitter = new MySseEmitter(userId);
 
