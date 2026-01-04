7b655732857ad7879acb3d8090e4b06e0cab990a
bhuism <bhuism@odee.net>
2026-01-04 23:35:07 +0100

    remove baseclass from SseEvent

--- src/main/java/nl/appsource/cardserver/model/SseEvent.java
+++ src/main/java/nl/appsource/cardserver/model/SseEvent.java
@@ -1,8 +1,10 @@
 package nl.appsource.cardserver.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.couchbase.core.mapping.Document;
 import org.springframework.data.couchbase.core.mapping.Expiry;
 
@@ -13,8 +15,12 @@ import java.util.concurrent.TimeUnit;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 @Expiry(expiry = 30, expiryUnit = TimeUnit.SECONDS)
-public class SseEvent extends BaseEntity {
+public class SseEvent {
+
+    @Id
+    private String id;
 
     private final String appIdentifier;
 
@@ -23,13 +29,5 @@ public class SseEvent extends BaseEntity {
     private final String event;
 
     private final Map<?, ?> data;
-
-    public SseEvent(final String id, final String appIdentifier, final String userId, final String event, final Map<?, ?> data) {
-        super(id);
-        this.appIdentifier = appIdentifier;
-        this.userId = userId;
-        this.event = event;
-        this.data = data;
-    }
 
 }
