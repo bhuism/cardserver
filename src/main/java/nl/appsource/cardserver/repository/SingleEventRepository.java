271adcefaa5febd145098e86071c3c3575ca58ba
Bas Huisman <bhuism@odee.net>
2026-01-25 12:04:18 +0100

    add some ScanConsistency
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- src/main/java/nl/appsource/cardserver/repository/SingleEventRepository.java
+++ src/main/java/nl/appsource/cardserver/repository/SingleEventRepository.java
@@ -3,16 +3,21 @@ package nl.appsource.cardserver.repository;
 import nl.appsource.cardserver.model.SingleEvent;
 import org.springframework.data.couchbase.repository.Query;
 import org.springframework.data.couchbase.repository.ReactiveCouchbaseRepository;
+import org.springframework.data.couchbase.repository.ScanConsistency;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
+
+import static com.couchbase.client.java.query.QueryScanConsistency.REQUEST_PLUS;
 
 @Repository
 public interface SingleEventRepository extends ReactiveCouchbaseRepository<SingleEvent, String> {
 
     @Query(value = "UPDATE #{#n1ql.bucket} USE KEYS $id SET lockedBy = $lockedBy WHERE META().id==$id AND lockedBy IS MISSING AND handledBy IS MISSING", readonly = false)
+    @ScanConsistency(query = REQUEST_PLUS)
     Mono<Void> lockById(String id, String lockedBy);
 
     @Query(value = "UPDATE #{#n1ql.bucket} USE KEYS $id SET handledBy = $handledBy WHERE META().id==$id AND lockedBy=$handledBy AND handledBy IS MISSING", readonly = false)
+    @ScanConsistency(query = REQUEST_PLUS)
     Mono<Void> handledBy(String id, String handledBy);
 
 }
--- src/main/java/nl/appsource/cardserver/repository/SseSessionRepository.java
+++ src/main/java/nl/appsource/cardserver/repository/SseSessionRepository.java
@@ -2,8 +2,11 @@ package nl.appsource.cardserver.repository;
 
 import nl.appsource.cardserver.model.SseSession;
 import org.springframework.data.couchbase.repository.ReactiveCouchbaseRepository;
+import org.springframework.data.couchbase.repository.ScanConsistency;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
+
+import static com.couchbase.client.java.query.QueryScanConsistency.REQUEST_PLUS;
 
 @Repository
 public interface SseSessionRepository extends ReactiveCouchbaseRepository<SseSession, String> {
@@ -12,6 +15,7 @@ public interface SseSessionRepository extends ReactiveCouchbaseRepository<SseSes
 //
 //    Mono<Boolean> existsByCreator(String userId);
 
+    @ScanConsistency(query = REQUEST_PLUS)
     Mono<SseSession> findByIdAndCreator(String s, String userId);
 
 //    @Query("SELECT count(*) > 0 FROM #{#n1ql.bucket} WHERE #{#n1ql.filter} AND (META().id=$appIdentifier OR creator=$userId) AND host=$hostname")
