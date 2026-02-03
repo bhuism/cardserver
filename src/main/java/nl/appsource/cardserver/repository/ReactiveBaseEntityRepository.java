2b16a94d26df4a4f17fe3ef75f0850acf05a86e0
Bas Huisman <bhuism@odee.net>
2026-02-03 15:38:47 +0100

    cleanups
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- src/main/java/nl/appsource/cardserver/repository/ReactiveBaseEntityRepository.java
+++ src/main/java/nl/appsource/cardserver/repository/ReactiveBaseEntityRepository.java
@@ -1,10 +1,12 @@
 package nl.appsource.cardserver.repository;
 
 import nl.appsource.cardserver.model.BaseEntity;
+import org.springframework.data.couchbase.repository.Query;
 import reactor.core.publisher.Mono;
 
 public interface ReactiveBaseEntityRepository<T extends BaseEntity> {
 
+    @Query("UPDATE #{#n1ql.bucket} USE KEYS $id SET updated=NOW_MILLIS() RETURNING meta().id")
     Mono<String> updateUpdated(String id);
 
 }
