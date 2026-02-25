74265907afab3d00c22abd1f54966b4305e1bed2
bhuism <bhuism@odee.net>
2026-02-25 23:16:14 +0100

    added vcs

---
+++ couchbase2redis/src/main/java/nl/appsource/cardserver/couchbase2redis/Couchbase2redisApplication.java
@@ -0,0 +1,13 @@
+package nl.appsource.cardserver.couchbase2redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Couchbase2redisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Couchbase2redisApplication.class, args);
+    }
+
+}
---
+++ couchbase2redis/src/test/java/nl/appsource/cardserver/couchbase2redis/Couchbase2redisApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.appsource.cardserver.couchbase2redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Couchbase2redisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
