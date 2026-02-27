c86aa192ac4927e87e051f5bd235cdda47542642
bhuism <bhuism@odee.net>
2026-02-27 11:36:39 +0100

    CardServerJwtModem moved

--- src/main/java/nl/appsource/cardserver/config/WebfluxSecurityConfig.java
+++ src/main/java/nl/appsource/cardserver/config/WebfluxSecurityConfig.java
@@ -2,7 +2,6 @@ package nl.appsource.cardserver.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import nl.appsource.cardserver.service.CardServerJwtModem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
