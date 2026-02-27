571bdadfac089e958fdb36f0d599593f136a0d36
bhuism <bhuism@odee.net>
2026-02-27 20:52:03 +0100

    checkstyle

--- stream/src/main/java/nl/appsource/cardserver/stream/config/WebfluxSecurityConfig.java
+++ stream/src/main/java/nl/appsource/cardserver/stream/config/WebfluxSecurityConfig.java
@@ -1,13 +1,10 @@
 package nl.appsource.cardserver.stream.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 import org.springframework.security.web.server.util.matcher.OrServerWebExchangeMatcher;
