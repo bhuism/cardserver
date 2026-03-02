7e9ec1682b84d768d96f52799f4b7b4e64e847f1
Bas Huisman <bhuism@odee.net>
2026-03-02 12:11:02 +0100

    cleanups
    Signed-off-by: Bas Huisman <bhuism@odee.net>

---
+++ stream/src/test/java/nl/appsource/cardserver/stream/controller/HttpRequestTests.java
@@ -0,0 +1,76 @@
+package nl.appsource.cardserver.stream.controller;
+
+import nl.appsource.cardserver.couchbase.repository.BoomRepository;
+import nl.appsource.cardserver.couchbase.repository.FeedbackRepository;
+import nl.appsource.cardserver.couchbase.repository.GameRepository;
+import nl.appsource.cardserver.couchbase.repository.SseEventRepository;
+import nl.appsource.cardserver.couchbase.repository.SseSessionRepository;
+import nl.appsource.cardserver.couchbase.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalManagementPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("citest")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class HttpRequestTests {
+
+    @LocalServerPort
+    private int port;
+
+    @LocalManagementPort
+    private int managementPort;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockitoBean
+    private GameRepository gameRepository;
+
+    @MockitoBean
+    private UserRepository userRepository;
+
+    @MockitoBean
+    private BoomRepository boomRepository;
+
+    @MockitoBean
+    private FeedbackRepository feedbackRepository;
+
+    @MockitoBean
+    private SseSessionRepository sseSessionRepository;
+
+    @MockitoBean
+    private SseEventRepository sseEventRepository;
+
+//    @Test
+//    void greetingShouldReturnDefaultMessage() {
+//        assertThat(this.webTestClient.get().uri("http://localhost:" + port + "/", String.class).exchange().expectStatus().isOk().expectBody(String.class).returnResult().getResponseBody())
+//            .contains("logo192.png");
+//    }
+
+    @Test
+    public void actuatorHealthShouldReturnDefaultMessage() {
+        assertThat(this.webTestClient.get().uri("http://localhost:" + managementPort + "/manage/health", String.class).exchange().expectStatus().isOk().expectBody(String.class).returnResult().getResponseBody())
+            .isEqualTo("{\"groups\":[\"liveness\",\"readiness\"],\"status\":\"UP\"}");
+    }
+
+    @Test
+    public void actuatorHealthLiveNessShouldReturnDefaultMessage() {
+        assertThat(this.webTestClient.get().uri("http://localhost:" + managementPort + "/manage/health/liveness", String.class).exchange().expectStatus().isOk().expectBody(String.class).returnResult().getResponseBody())
+            .isEqualTo("{\"status\":\"UP\"}");
+    }
+
+    @Test
+    public void actuatorHealthReadinessShouldReturnDefaultMessage() {
+        assertThat(this.webTestClient.get().uri("http://localhost:" + managementPort + "/manage/health/readiness", String.class).exchange().expectStatus().isOk().expectBody(String.class).returnResult().getResponseBody())
+            .isEqualTo("{\"status\":\"UP\"}");
+    }
+}
