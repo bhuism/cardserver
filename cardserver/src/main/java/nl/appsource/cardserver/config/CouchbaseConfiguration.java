c11210e6fa6da249d52784affdfd1c055a382ebd
Bas Huisman <bhuism@odee.net>
2026-03-02 17:45:22 +0100

    checkstyle
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- cardserver/src/main/java/nl/appsource/cardserver/config/CouchbaseConfiguration.java
+++ cardserver/src/main/java/nl/appsource/cardserver/config/CouchbaseConfiguration.java
@@ -3,7 +3,7 @@ package nl.appsource.cardserver.config;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Profile( "!citest")
-@Import( nl.appsource.cardserver.couchbase.config.CouchbaseConfiguration.class)
+@Profile("!citest")
+@Import(nl.appsource.cardserver.couchbase.config.CouchbaseConfiguration.class)
 public class CouchbaseConfiguration {
 }
