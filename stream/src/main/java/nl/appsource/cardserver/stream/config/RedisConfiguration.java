a1e3e655a409108e4975451d9113764ad367df00
Bas Huisman <bhuism@odee.net>
2026-03-03 09:32:51 +0100

    add pubsub to stream
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- stream/src/main/java/nl/appsource/cardserver/stream/config/RedisConfiguration.java
+++ stream/src/main/java/nl/appsource/cardserver/stream/config/RedisConfiguration.java
@@ -1,11 +1,20 @@
 package nl.appsource.cardserver.stream.config;
 
+import nl.appsource.cardserver.openapi.service.PubSubService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer;
 
 @Configuration
 @Import(nl.appsource.cardserver.openapi.config.RedisConfiguration.class)
 @Profile("!citest")
 public class RedisConfiguration {
+
+    @Bean
+    PubSubService pubSubService(final ReactiveRedisMessageListenerContainer reactiveRedisMessageListenerContainer) {
+        return new PubSubService(reactiveRedisMessageListenerContainer);
+    }
+    
 }
