823f33551795321177c43ac8683e456d515af60b
Bas Huisman <bhuism@odee.net>
2026-03-12 14:41:01 +0100

    cleanups
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- admin/src/main/java/nl/appsource/cardserver/admin/AdminApplication.java
+++ admin/src/main/java/nl/appsource/cardserver/admin/AdminApplication.java
@@ -5,6 +5,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -31,7 +32,7 @@ public class AdminApplication {
                 .authenticated())
             .formLogin(spec -> spec.loginPage(adminServerProperties.getContextPath() + "/login")
                 .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler(adminServerProperties.getContextPath())))
-            .logout(spec -> spec.logoutUrl(adminServerProperties.getContextPath() + "/logout"))
+            .logout(Customizer.withDefaults())
             .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
             .build();
