3d3fd2535ab4dd9d85a7f4dc69c7d538d07deeca
Bas Huisman <bhuism@odee.net>
2026-03-12 11:27:26 +0100

    add admin
    Signed-off-by: Bas Huisman <bhuism@odee.net>

---
+++ admin/src/main/java/nl/appsource/cardserver/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package nl.appsource.cardserver.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
