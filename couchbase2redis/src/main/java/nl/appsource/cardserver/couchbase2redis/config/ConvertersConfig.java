da22cc67c57ac8003db4efd1c7f1f9019a48ccbd
Bas Huisman <bhuism@odee.net>
2026-03-18 14:25:46 +0100

    remove couchbase2redis
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- couchbase2redis/src/main/java/nl/appsource/cardserver/couchbase2redis/config/ConvertersConfig.java
+++
@@ -1,9 +0,0 @@
-package nl.appsource.cardserver.couchbase2redis.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@Configuration
-@Import(nl.appsource.cardserver.converters.config.ConvertersConfig.class)
-public class ConvertersConfig {
-}
