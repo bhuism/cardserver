a7d059f7b3ebd833b82713c3f4a92a61ca8d2db0
Bas Huisman <bhuism@odee.net>
2026-07-13 14:42:18 +0200

    fix name
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- openapi/src/main/java/nl/appsource/cardserver/openapi/config/KnativeProperties.java
+++ openapi/src/main/java/nl/appsource/cardserver/openapi/config/KnativeProperties.java
@@ -16,6 +16,6 @@ public class KnativeProperties {
     /**
      * The source of the events.
      */
-    @Value("${spring.application.name}")
+    @Value("#{'${spring.application.name}'.replace(' ', '_')}")
     private String source;
 }
