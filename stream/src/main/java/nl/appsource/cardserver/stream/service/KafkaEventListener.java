64ce168eaa4b30b621f045dd8f3b9d00b94fc905
bhuism <bhuism@odee.net>
2026-07-15 23:17:35 +0200

    feat: add production profile to KafkaEventListener

--- stream/src/main/java/nl/appsource/cardserver/stream/service/KafkaEventListener.java
+++ stream/src/main/java/nl/appsource/cardserver/stream/service/KafkaEventListener.java
@@ -2,11 +2,13 @@ package nl.appsource.cardserver.stream.service;
 
 import lombok.extern.slf4j.Slf4j;
 import nl.appsource.cardserver.stream.model.CouchbaseEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@Profile("production")
 public class KafkaEventListener {
 
     @KafkaListener(topics = "couchbase-cardserver-events")
