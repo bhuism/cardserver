86f9f5d514459383cacdc88737ce810b1888f723
bhuism <bhuism@odee.net>
2026-07-19 14:19:59 +0200

    refactor: remove unused KafkaTopics import from AiWorker class

--- aiplayer/src/main/java/nl/appsource/cardserver/aiplayer/service/AiWorker.java
+++ aiplayer/src/main/java/nl/appsource/cardserver/aiplayer/service/AiWorker.java
@@ -8,7 +8,6 @@ import nl.appsource.cardserver.couchbase.utils.GameEngine;
 import nl.appsource.cardserver.couchbase.utils.GameEngineImpl;
 import nl.appsource.cardserver.model.Card;
 import nl.appsource.cardserver.model.Game;
-import nl.appsource.cardserver.openapi.config.KafkaTopics;
 import nl.appsource.cardserver.openapi.service.RedisStreamService;
 import nl.appsource.generated.openapi.model.GameEvent;
 import org.springframework.context.annotation.Profile;
