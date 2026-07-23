b12b918798cde1a17bc54f7538a50ceec477ac00
Bas Huisman <bhuism@odee.net>
2026-07-23 20:51:16 +0200

    migrate to spring-boot-starter-kafka, add environment-specific Kafka configurations, and introduce KafkaConfig classes for producer-consumer setup
    Signed-off-by: Bas Huisman <bhuism@odee.net>

---
+++ openapi/src/main/java/nl/appsource/cardserver/openapi/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package nl.appsource.cardserver.openapi.config;
+
+import nl.appsource.cardserver.openapi.service.KafkaSender;
+import nl.appsource.cardserver.openapi.service.KafkaSenderImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import tools.jackson.databind.json.JsonMapper;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public KafkaSender kafkaSender(final KafkaTemplate kafkaTemplate, final JsonMapper jsonMapper) {
+        return new KafkaSenderImpl(kafkaTemplate, jsonMapper);
+    }
+
+}
