797fb69085d719a54f6f910407e5afd2e2a31a04
Bas Huisman <bhuism@odee.net>
2026-07-23 21:41:59 +0200

    refactor KafkaEventListenerImpl to improve readability by restructuring conditional blocks and aligning indentation
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- aiplayer/src/main/java/nl/appsource/cardserver/aiplayer/config/KafkaEventListenerImpl.java
+++ aiplayer/src/main/java/nl/appsource/cardserver/aiplayer/config/KafkaEventListenerImpl.java
@@ -39,21 +39,21 @@ public class KafkaEventListenerImpl {
             log.info("Got Kafka event: documentId={}", documentId);
 
             if (Game.class.getName().equals(document.get("_class").asString())) {
+
                 log.info("Got Kafka event: documentId={}, document={}", documentId, document);
+
+                final Game game = jsonMapper.convertValue(document, Game.class);
+
+                final GameEngineImpl gameEngine = new GameEngineImpl(game);
+
+                if (gameEngine.isAiSay()) {
+                    final String aiSayPlayer = game.getPlayers().get(gameEngine.calcWhoSay());
+                    aiWorker.say(game.getId(), aiSayPlayer).block();
+                } else if (gameEngine.isAiTurn()) {
+                    final String aiPlayPlayer = game.getPlayers().get(gameEngine.calcWhoHasTurn());
+                    aiWorker.playCard(game.getId(), aiPlayPlayer).block();
+                }
             }
-
-            final Game game = jsonMapper.convertValue(document, Game.class);
-
-            final GameEngineImpl gameEngine = new GameEngineImpl(game);
-
-            if (gameEngine.isAiSay()) {
-                final String aiSayPlayer = game.getPlayers().get(gameEngine.calcWhoSay());
-                aiWorker.say(game.getId(), aiSayPlayer).block();
-            } else if (gameEngine.isAiTurn()) {
-                final String aiPlayPlayer = game.getPlayers().get(gameEngine.calcWhoHasTurn());
-                aiWorker.playCard(game.getId(), aiPlayPlayer).block();
-            }
-
 
         } catch (final Exception e) {
             log.error("Error processing Kafka event: documentId={}", documentId, e);
