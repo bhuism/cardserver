19f86a6d5d254ac4e648ae8594c9e6a0a3adb94b
bhuism <bhuism@odee.net>
2026-08-09 13:48:19 +0200

    refactor: reorder Flux.merge elements in SseEmitterRepositoryImpl for consistent event handling flow

--- stream/src/main/java/nl/appsource/cardserver/stream/service/SseEmitterRepositoryImpl.java
+++ stream/src/main/java/nl/appsource/cardserver/stream/service/SseEmitterRepositoryImpl.java
@@ -131,7 +131,7 @@ public class SseEmitterRepositoryImpl implements SseEmitterRepository {
 
         return //friendsMono
 //            .thenMany(
-            concat(just(hello(appIdentifier)), pingSink, Flux.merge(asyncCache, kafkaEventListener.kafkaStreams()))
+            concat(just(hello(appIdentifier)), Flux.merge(pingSink, asyncCache, kafkaEventListener.kafkaStreams()))
                 .onBackpressureBuffer(1024, BufferOverflowStrategy.DROP_OLDEST)
                 .doFinally(signalType -> {
                     log.info("{} doFinally() signalType={} appIdentifier={} userId={}", remoteAddress, signalType, appIdentifier, userId);
