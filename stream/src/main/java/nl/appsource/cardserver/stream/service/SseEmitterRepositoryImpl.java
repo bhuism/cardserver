8875cd9e93d58b494cd97ef96a9b4b9393b94df1
bhuism <bhuism@odee.net>
2026-08-09 13:29:50 +0200

    refactor: reorder Flux.concat elements in SseEmitterRepositoryImpl for consistent initialization flow

--- stream/src/main/java/nl/appsource/cardserver/stream/service/SseEmitterRepositoryImpl.java
+++ stream/src/main/java/nl/appsource/cardserver/stream/service/SseEmitterRepositoryImpl.java
@@ -131,7 +131,7 @@ public class SseEmitterRepositoryImpl implements SseEmitterRepository {
 
         return //friendsMono
 //            .thenMany(
-            concat(just(hello(appIdentifier)), just(ping(0)), kafkaEventListener.kafkaStreams(), asyncCache, pingSink)
+            concat(just(hello(appIdentifier)), just(ping(0)), pingSink, asyncCache, kafkaEventListener.kafkaStreams())
                 .onBackpressureBuffer(1024, BufferOverflowStrategy.DROP_OLDEST)
                 .doFinally(signalType -> {
                     log.info("{} doFinally() signalType={} appIdentifier={} userId={}", remoteAddress, signalType, appIdentifier, userId);
