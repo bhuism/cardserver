7492e6fd49b52304cc926e2560a786796ce994e6
Bas Huisman <bhuism@odee.net>
2026-08-10 12:03:02 +0200

    fix incorrect closing parentheses in `UserRepository` N1QL queries (`findIncomingInvites`, `searchInvitees`)
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- couchbase/src/main/java/nl/appsource/cardserver/couchbase/repository/UserRepository.java
+++ couchbase/src/main/java/nl/appsource/cardserver/couchbase/repository/UserRepository.java
@@ -12,10 +12,10 @@ public interface UserRepository extends ReactiveCouchbaseRepository<User, String
 
     Mono<User> findByEmail(String email);
 
-    @Query(value = "SELECT meta(#{#n1ql.bucket}).id FROM #{#n1ql.bucket} WHERE #{#n1ql.filter} AND meta(#{#n1ql.bucket}).id) != $userId AND ANY inv IN invites SATISFIES inv = $userId END ORDER BY updated DESC", readonly = true)
+    @Query(value = "SELECT meta(#{#n1ql.bucket}).id FROM #{#n1ql.bucket} WHERE #{#n1ql.filter} AND meta(#{#n1ql.bucket}).id != $userId AND ANY inv IN invites SATISFIES inv = $userId END ORDER BY updated DESC", readonly = true)
     Flux<String> findIncomingInvites(String userId);
 
-    @Query(value = "#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND meta(#{#n1ql.bucket}).id) != $userId AND (LOWER(email)=LOWER($searchString) OR LOWER(name)=LOWER($searchString) OR LOWER(displayName)=LOWER($searchString)) OR META().id=$searchString ORDER BY updated DESC", readonly = true)
+    @Query(value = "#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND meta(#{#n1ql.bucket}).id != $userId AND (LOWER(email)=LOWER($searchString) OR LOWER(name)=LOWER($searchString) OR LOWER(displayName)=LOWER($searchString)) OR META().id=$searchString ORDER BY updated DESC", readonly = true)
     Flux<User> searchInvitees(String userId, String searchString);
 
     Mono<Boolean> existsByDisplayNameAndIdNot(String displayName, String id);
